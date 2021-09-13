4bd8a3e964aae56263a8d991dc7fd992d78bedee
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2021-09-13 12:03:22 +0800

    7

--- massfill.js
+++ massfill.js
@@ -1,11 +1,18 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
+const id = require("./id.json")
+const id_Length = Object.keys(id).length
+console.log(id_Length)
 
+function float(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
+for(const property in id){
   (async function example() {
     let driver = await new Builder().forBrowser('firefox').build();
     try {
       await driver.get('https://webap1.kshs.kh.edu.tw/kshsSSO/publicWebAP/bodyTemp/index.aspx');
-      await driver.findElement(By.name('ctl00$ContentPlaceHolder1$txtId')).sendKeys("ur id", Key.RETURN);
+      await driver.findElement(By.name('ctl00$ContentPlaceHolder1$txtId')).sendKeys(id[property], Key.RETURN);
       await driver.sleep(200)
       await driver.findElement(By.id('ContentPlaceHolder1_rbType_1')).click()
       await driver.findElement(By.name('ctl00$ContentPlaceHolder1$ddl1')).sendKeys("35", Key.RETURN);
@@ -16,7 +23,7 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
       //await driver.quit();
     }
   })();
-
+}
 
 
 
