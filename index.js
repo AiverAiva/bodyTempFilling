126230ffb010a3007d59999f8abcd38357046a55
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2022-05-02 20:36:08 +0800

    .

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const puppeteer = require('puppeteer');
 
+(async () => {
+  const browser = await puppeteer.launch({ headless: false })
+  const [page] = await browser.pages();
+  await page.goto("https://webap1.kshs.kh.edu.tw/kshsSSO/publicWebAP/bodyTemp/index.aspx");
+  await page.type('input[name="ctl00$ContentPlaceHolder1$txtId"]', "wqeqwassaf")
+  // await page.click();
+  
+  // await browser.close();
 
-  (async function example() {
-    let driver = await new Builder().forBrowser('firefox').build();
-    try {
-      await driver.get('https://webap1.kshs.kh.edu.tw/kshsSSO/publicWebAP/bodyTemp/index.aspx');
-      await driver.findElement(By.name('ctl00$ContentPlaceHolder1$txtId')).sendKeys("ur id", Key.RETURN);
-      await driver.sleep(200)
-      await driver.findElement(By.id('ContentPlaceHolder1_rbType_1')).click()
-      await driver.findElement(By.name('ctl00$ContentPlaceHolder1$ddl1')).sendKeys("35", Key.RETURN);
-      await driver.findElement(By.name('ctl00$ContentPlaceHolder1$ddl2')).sendKeys(float(9), Key.RETURN);
-      await driver.findElement(By.name('ctl00$ContentPlaceHolder1$ddl3')).sendKeys("正常", Key.RETURN);
-      await driver.findElement(By.name('ctl00$ContentPlaceHolder1$btnId0')).click()
-    } finally {
-      //await driver.quit();
-    }
-  })();
-
-
-
-
-
-
+})();
 
